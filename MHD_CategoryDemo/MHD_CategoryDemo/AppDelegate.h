baad1f76a1ddf45abd6bdcb00e7c5e38434a20f2
Ardee <goobama@outlook.com>
2018-05-21 10:30:34 -0700

    创建demo,应用分类工具

---
+++ MHD_CategoryDemo/MHD_CategoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MHD_CategoryDemo
+//
+//  Created by Ardee on 2018/5/21.
+//  Copyright © 2018年 Ardee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MHD_CategoryDemo/MHD_CategoryDemoTests/MHD_CategoryDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  MHD_CategoryDemoTests.m
+//  MHD_CategoryDemoTests
+//
+//  Created by Ardee on 2018/5/21.
+//  Copyright © 2018年 Ardee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MHD_CategoryDemoTests : XCTestCase
+
+@end
+
+@implementation MHD_CategoryDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
